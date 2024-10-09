ce6a3e0167672007b92bcbb76a35ac3008efea54
Fabian Stiven Valencia Cordoba <fabian.cordoba@correounivalle.edu.co>
2024-10-09 10:04:22 -0500

    Sesion 9 middle implemented

--- sesion 9/sesion-9-pi-2024-2-start/src/pages/home/lights/Lights.jsx
+++ sesion 9/sesion-9-pi-2024-2-start/src/pages/home/lights/Lights.jsx
@@ -3,13 +3,14 @@ const Lights = () => {
     <>
       <ambientLight intensity={0.5} />
       <directionalLight position={[0, 10, 10]} intensity={2} castShadow 
-        shadow-camera-left = {-50}
-        shadow-camera-right = {50}
-        shadow-camera-top = {50}
-        shadow-camera-bottom = {-50}
+        shadow-camera-left={-50}
+        shadow-camera-right={50}
+        shadow-camera-top={50}
+        shadow-camera-bottom={-50}
         shadow-camera-far={50}
         shadow-mapSize-width={2048}
         shadow-mapSize-height={2048}
+        shadow-normalBias={0.5}
       />
     </>
   );
